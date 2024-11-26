974b0e8ef164991429352b71a9f14535af695505
MrTinker64 <avi.micah@gmail.com>
2024-11-26 11:34:54 -0800

    remove uneccessary imports

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,9 +13,6 @@
 
 package frc.robot;
 
-// import com.ctre.phoenix6.SignalLogger;
-// import edu.wpi.first.math.geometry.Pose2d;
-// import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PowerDistribution;
