0223686c7dd15aadd9847dc9589a8280668a5029
Kyle Lin <klin28@urbanschool.org>
2024-12-07 15:54:51 -0800

    add doubles to intakeIO

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems.intake;
 public class IntakeIO {
     
     public static class IntakeIOInputs{
-        public double appliedVolts = 0.0;
-        public double currentAmps = 0.0;
+        
     }
 }
