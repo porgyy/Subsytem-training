0a91ebd7d35883e0f1a6182a6785a02e5e9c6c91
Lucas Lee <llee28@urbanschool.org>
2024-12-07 15:56:13 -0800

    add appliedvolts and currentamps variables

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.intake;
 public class IntakeIO {
     
     public static class IntakeIOInputs{
-        
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
     }
 }
