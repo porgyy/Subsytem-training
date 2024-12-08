316593753fb354c1e510894855194d3155e5b4ab
Lucas Lee <llee28@urbanschool.org>
2024-12-07 16:00:40 -0800

    change class to interface

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -1,9 +1,11 @@
 package frc.robot.subsystems.intake;
+import org.littletonrobotics.junction.AutoLog;
 
-public class IntakeIO {
-    
+public interface IntakeIO {
+    @AutoLog
     public static class IntakeIOInputs{
         public double appliedVolts = 0.0;
         public double currentAmps = 0.0;
     }
+    public default void stopIntake() {}
 }
